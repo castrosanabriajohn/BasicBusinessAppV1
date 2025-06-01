3f8d9b5d8419b5ebd47223544aebd77223d6ddfd
castrosanabriajohn <castrosabriajohn@gmail.com>
2025-06-01 01:17:51 -0600

    add error handling middleware filter

--- BasicBusinessApp.Api/Program.cs
+++ BasicBusinessApp.Api/Program.cs
@@ -1,3 +1,5 @@
+using BasicBusinessApp.Api.Filters;
+
 using BasicBusinessApp.Application;
 using BasicBusinessApp.Infrastructure;
 
@@ -6,11 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers(); 
     app.Run(); 
